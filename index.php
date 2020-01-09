1ca5f528b61f915597f58a2e814f1e6585c352b8
waenurma <46836897+waenurma@users.noreply.github.com>
2020-01-09 14:02:38 +0700

    test

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 <?php
-echo 'bbbb';
+
+$channelAccessToken = 'exRwq1i1noogIKE8x9QpmYH8PlQQdSvCjBEeoQfy+sCbKkLHNV3Kol5ZxfuCebtuRyHunNm6/KGAVw+uDgy6GQEAeKsAhLGAIpJCYMLvxVWVX2b4o8DN0z03MVgp1TC2JsjIEQPXRqWxua9JrPIVfwdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'aa79f5f6f04e775f836bf54644526aed';
+//echo 'bbbb';
