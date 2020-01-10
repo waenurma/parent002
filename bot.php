9670a71647533b10be2ef7ba4342d543d64dca7e
waenurma <46836897+waenurma@users.noreply.github.com>
2020-01-10 15:35:20 +0700

    05

--- bot.php
+++ bot.php
@@ -5,44 +5,41 @@ $ACCESS_TOKEN = 'exRwq1i1noogIKE8x9QpmYH8PlQQdSvCjBEeoQfy+sCbKkLHNV3Kol5ZxfuCebt
 $channelSecret = 'aa79f5f6f04e775f836bf54644526aed';
 // Set HEADER
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+$arrPostData = array();
 // Get request content
 $request = file_get_contents('php://input');
 // Decode JSON to Array
 $request_array = json_decode($request, true);
 
-$arrPostData = array();
-
-function send_reply_message($url, $post_header, $post_body)
-
-
-//request ไปถึง Line API
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $API_URL);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $POST_HEADER);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$result = curl_exec($ch);
-curl_close($ch);
-
 //Get ข้อมูลที่ได้จากการที่ User ที่มีการกระทำใน Channel ตัวอย่างเช่น การพิมพ์ข้อความ การส่งสติกเกอร์
-if ( sizeof($request_array['events']) > 0 ) {
+if (sizeof($request_array['events']) > 0) {
     foreach ($request_array['events'] as $event) {
-    
-    $reply_message = '';
-    $reply_token = $event['replyToken'];
-    $data = [
-       'replyToken' => $reply_token,
-       'messages' => [
-          ['type' => 'text', 
-           'text' => json_encode($request_array)]
-       ]
-    ];
-    $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-    $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-    echo "Result: ".$send_result."\r\n";
- }
+
+        $reply_message = '';
+        $reply_token = $event['replyToken'];
+        $data = [
+            'replyToken' => $reply_token,
+            'messages' => [
+                ['type' => 'text',
+                    'text' => json_encode($request_array)],
+            ],
+        ];
+        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+        $send_result = send_reply_message($API_URL . '/reply', $POST_HEADER, $post_body);
+        echo "Result: " . $send_result . "\r\n";
+    }
 }
 echo "OK";
+
+// ส่วนของการส่งการแจ้งเตือนผ่านฟังก์ชั่น cURL
+function send_reply_message($url, $post_header, $post_body)
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $API_URL);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $POST_HEADER);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec($ch);
+    curl_close($ch);
