8faa7afef200f855685720fe847950211dfdacac
waenurma <46836897+waenurma@users.noreply.github.com>
2020-01-10 11:26:19 +0700

    03

--- bot.php
+++ bot.php
@@ -1,65 +1,22 @@
 <?php
+
 //echo 'ทำไงดี โว้ยยยยยยยยยยยยย';
+
 $API_URL = 'https://api.line.me/v2/bot/message';
 $ACCESS_TOKEN = 'exRwq1i1noogIKE8x9QpmYH8PlQQdSvCjBEeoQfy+sCbKkLHNV3Kol5ZxfuCebtuRyHunNm6/KGAVw+uDgy6GQEAeKsAhLGAIpJCYMLvxVWVX2b4o8DN0z03MVgp1TC2JsjIEQPXRqWxua9JrPIVfwdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'aa79f5f6f04e775f836bf54644526aed';
-$content = file_get_contents('php://input'); // Get request content
-$arrJson = json_decode($content, true);
+// Set HEADER
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+// Get request content
+$request = file_get_contents('php://input');
+// Decode JSON to Array
+$request_array = json_decode($request, true);
 
 $arrPostData = array();
 
-$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
-//ส่งidผู้ใช้
-if ($arrJson['events'][0]['message']['text'] == "สวัสดี") {
-    $arrPostData = array();
-    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-    $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ" . $arrJson['events'][0]['source']['userId'];
-
-//ส่งบัตรปชช
-} else if ($arrJson['events'][0]['message']['text'] == "1951234567890") {
-    $arrPostData = array();
-    $data = $arrJson['events'][0]['message']['text'];
-
-    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-    $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = strlen($data); //เป็นการนับ
-
-//เลขสมาชิก
-} else if ($arrJson['events'][0]['message']['text'] == "1234567890") {
-    $arrPostData = array();
-    $data = $arrJson['events'][0]['message']['text'];
-    $cutstring = substr($data, 5, 10); //เป็นการตัดข้อความ
-
-    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-    $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = $cutstring;
-
-//ตอบตรงคำถาม
-} else if ($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง") {
-    $arrPostData = array();
-    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-    $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
-
-//ตอบกลับที่เราพิมพ์
-} else if ($arrJson['events'][0]['message']['text'] == "เหมืนเดิม") {
-    $arrPostData = array();
-    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-    $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = $arrJson['events'][0]['message']['text'];
-
-//ถ้าข้อความไม่ตรงเงือนไขทั้งหมดจะตอบเงือนไขสุดท้าย
-} else {
-    $arrPostData = array();
-    $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-    $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = "กรอกข้อมูลไม่ถูกต้องกรุณา กรอกข้อมูลใหม่!!!";
-
-}
-
-// -----------------------------------------------------------------------------------------------------
+function send_reply_message($url, $post_header, $post_body)
+//request ไปถึง Line API
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $API_URL);
 curl_setopt($ch, CURLOPT_HEADER, false);
@@ -70,3 +27,23 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close($ch);
+
+//Get ข้อมูลที่ได้จากการที่ User ที่มีการกระทำใน Channel ตัวอย่างเช่น การพิมพ์ข้อความ การส่งสติกเกอร์
+if ( sizeof($request_array['events']) > 0 ) {
+    foreach ($request_array['events'] as $event) {
+    
+    $reply_message = '';
+    $reply_token = $event['replyToken'];
+    $data = [
+       'replyToken' => $reply_token,
+       'messages' => [
+          ['type' => 'text', 
+           'text' => json_encode($request_array)]
+       ]
+    ];
+    $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+    $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+    echo "Result: ".$send_result."\r\n";
+ }
+}
+echo "OK";
--- index.php
+++ index.php
@@ -1,3 +1,70 @@
+<?php session_start();?>
 <?php
+include 'h.php';
+?>
+<style type="text/css">
 
-echo 'i am bot';
+#btn{
+width:100%;
+}
+</style>
+<title>หน้าหลัก</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet" />
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" />
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+  <style type="text/css">
+  </style>
+  <style type="text/css"> body {
+      font-family: 'Kanit', sans-serif;
+        background: linear-gradient(to top,#6699CC 0%, #6699CC 100%);
+        color: black;
+}</style>
+  <style>
+      body {
+        font-family: 'Kanit', sans-serif;
+      }
+    </style>
+<div class="container" style="padding-top:100px">
+  <div class="row">
+    <div class="col-md-4"></div>
+    <div class="col-md-4" style="background-color:#6699FF">
+      <h3 align="center">
+      <span class="glyphicon glyphicon-lock"> </span>
+      ลงชื่อเข้าใช้งาน </h3>
+      <form  name="formlogin" action="save.php" method="POST" id="login" class="form-horizontal">
+        <div class="form-group">
+        <input type="hidden"  name="id_login" class="form-control" required placeholder="บัตรประชาชน" />
+        <div class="form-group">
+        <div class="col-sm-12">
+            <input type="text"  name="entity" class="form-control" required placeholder="บัตรประชาชน" />
+          </div>
+        </div>
+        <div class="form-group">
+        <div class="col-sm-12">
+            <input type="date"  name="bdate" class="form-control" required placeholder="วันเดือนปี" />
+          </div>
+        </div>
+        <div class="form-group">
+         <div class="col-sm-12">
+            <input type="text"  name="tell" class="form-control" required placeholder="เบอร์โทรศัพท์" />
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-12">
+            <button type="submit" name="insert" class="btn btn-success" id="btn">
+            <span class="glyphicon glyphicon-log-in"> </span>
+             ลงทะเบียน </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
