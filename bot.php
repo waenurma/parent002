86fae3c4d6723761925cd0d9d286876852c97184
waenurma <46836897+waenurma@users.noreply.github.com>
2020-01-13 14:51:35 +0700

    video33

--- bot.php
+++ bot.php
@@ -10,75 +10,75 @@
     
     //รับข้อความจากผู้ใช้
     $message = $arrayJson['events'][0]['message']['text'];
-#ตัวอย่าง Message Type "Text"
-    if($message == "สวัสดี"){
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
-        replyMsg($arrayHeader,$arrayPostData);
-    }
-    #ตัวอย่าง Message Type "Sticker"
-    else if($message == "ฝันดี"){
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "sticker";//ตอบกลับข้อความ
-        $arrayPostData['messages'][0]['packageId'] = "2";
-        $arrayPostData['messages'][0]['stickerId'] = "46";
-        replyMsg($arrayHeader,$arrayPostData);
-    }
-    #ตัวอย่าง Message Type "Image"
-    else if($message == "รูปน้องแมว"){
-        $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "image";
-        $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
-        $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
-        replyMsg($arrayHeader,$arrayPostData);
-    }
-
-    #ตัวอย่าง Message Type "Text + Sticker ใน 1 ครั้ง"
-    else if($message == "ลาก่อน"){
-        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
-        $arrayPostData['messages'][1]['type'] = "sticker";
-        $arrayPostData['messages'][1]['packageId'] = "1";
-        $arrayPostData['messages'][1]['stickerId'] = "131";
-        replyMsg($arrayHeader,$arrayPostData);
-    }
-
+ //รับข้อความจากผู้ใช้
+ $message = $arrayJson['events'][0]['message']['text'];
+ #ตัวอย่าง Message Type "Text"
+     if($message == "สวัสดี"){
+         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+         $arrayPostData['messages'][0]['type'] = "text";
+         $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+         replyMsg($arrayHeader,$arrayPostData);
+     }
+     #ตัวอย่าง Message Type "Sticker"
+     else if($message == "ฝันดี"){
+         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+         $arrayPostData['messages'][0]['type'] = "sticker";
+         $arrayPostData['messages'][0]['packageId'] = "2";
+         $arrayPostData['messages'][0]['stickerId'] = "46";
+         replyMsg($arrayHeader,$arrayPostData);
+     }
+     #ตัวอย่าง Message Type "Image"
+     else if($message == "รูปน้องแมว"){
+         $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
+         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+         $arrayPostData['messages'][0]['type'] = "image";
+         $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
+         $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
+         replyMsg($arrayHeader,$arrayPostData);
+     }
      #ตัวอย่าง Message Type "Location"
      else if($message == "พิกัดสยามพารากอน"){
+         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+         $arrayPostData['messages'][0]['type'] = "location";
+         $arrayPostData['messages'][0]['title'] = "สยามพารากอน";
+         $arrayPostData['messages'][0]['address'] =   "13.7465354,100.532752";
+         $arrayPostData['messages'][0]['latitude'] = "13.7465354";
+         $arrayPostData['messages'][0]['longitude'] = "100.532752";
+         replyMsg($arrayHeader,$arrayPostData);
+     }
+     #ตัวอย่าง Message Type "Text + Sticker ใน 1 ครั้ง"
+     else if($message == "ลาก่อน"){
+         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+         $arrayPostData['messages'][0]['type'] = "text";
+         $arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
+         $arrayPostData['messages'][1]['type'] = "sticker";
+         $arrayPostData['messages'][1]['packageId'] = "1";
+         $arrayPostData['messages'][1]['stickerId'] = "131";
+         replyMsg($arrayHeader,$arrayPostData);
+        
+     }
+
+     #ตัวอย่าง Message Type "Video"
+     else if($message == "video"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "location";
-        $arrayPostData['messages'][0]['title'] = "สยามพารากอน";
-        $arrayPostData['messages'][0]['address'] =   "13.7465354,100.532752";
-        $arrayPostData['messages'][0]['latitude'] = "13.7465354";
-        $arrayPostData['messages'][0]['longitude'] = "100.532752";
+        $arrayPostData['messages'][0]['type'] = "video";
+        $arrayPostData['messages'][0]['title'] = "video";
+        $arrayPostData['messages'][0]['originalContentUrl'] = "";//ใส่ url ของ video ที่ต้องการส่ง
+        $arrayPostData['messages'][0]['previewImageUrl'] = "";//ใส่รูป preview ของ video
         replyMsg($arrayHeader,$arrayPostData);
     }
-    #ตัวอย่าง Message Type "Video"
-     else  if($message == "video"){
-      $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-      $arrayPostData['messages'][0]['type'] = "video";
-      $arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
-      $arrayPostData['messages'][0]['originalContentUrl'] = "https://www.youtube.com/watch?v=xtjDFuCCN7g&list=RDILU9NbWn4t0&index=23";//ใส่ url ของ video ที่ต้องการส่ง
-      $arrayPostData['messages'][0]['previewImageUrl'] = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";//ใส่รูป preview ของ video
-      replyMsg($arrayHeader,$arrayPostData);
-
-      
-  }
-function replyMsg($arrayHeader,$arrayPostData){
-        $strUrl = "https://api.line.me/v2/bot/message/reply";
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,$strUrl);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
-        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        $result = curl_exec($ch);
-        curl_close ($ch);
-    }
-   exit;
-?>
+ function replyMsg($arrayHeader,$arrayPostData){
+         $strUrl = "https://api.line.me/v2/bot/message/reply";
+         $ch = curl_init();
+         curl_setopt($ch, CURLOPT_URL,$strUrl);
+         curl_setopt($ch, CURLOPT_HEADER, false);
+         curl_setopt($ch, CURLOPT_POST, true);
+         curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+         curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+         $result = curl_exec($ch);
+         curl_close ($ch);
+     }
+    exit;
+ ?>
