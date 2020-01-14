dd416f1e719fa0d394ed6ba85dc932775bc82c4d
waenurma <46836897+waenurma@users.noreply.github.com>
2020-01-14 12:10:43 +0700

    rich menu

--- bot.php
+++ bot.php
@@ -72,11 +72,37 @@
         replyMsg($arrayHeader,$arrayPostData);
     }
 
-     else if(isset($dataPostback['action']) && $dataPostback['action']=="get_richmenu"){
-        $respRichMenu = $bot->getRichMenu($dataPostback['richMenuId']);
-        $moreResult = $respRichMenu->getRawBody();
-        $result = json_decode($respRichMenu->getRawBody(),TRUE); 
-    }
+    function createNewRichmenu($channelAccessToken) {
+        $sh = <<< EOF
+        curl -X POST \
+        -H 'Authorization: Bearer $channelAccessToken' \
+        -H 'Content-Type:application/json' \
+        -d '{"size": {"width": 2500,"height": 1686},"selected": false,"name": "Controller","chatBarText": "Controller","areas": [{"bounds": {"x": 551,"y": 325,"width": 321,"height": 321},"action": {"type": "message","text": "up"}},{"bounds": {"x": 876,"y": 651,"width": 321,"height": 321},"action": {"type": "message","text": "right"}},{"bounds": {"x": 551,"y": 972,"width": 321,"height": 321},"action": {"type": "message","text": "down"}},{"bounds": {"x": 225,"y": 651,"width": 321,"height": 321},"action": {"type": "message","text": "left"}},{"bounds": {"x": 1433,"y": 657,"width": 367,"height": 367},"action": {"type": "message","text": "btn b"}},{"bounds": {"x": 1907,"y": 657,"width": 367,"height": 367},"action": {"type": "message","text": "btn a"}}]}' https://api.line.me/v2/bot/richmenu;
+      EOF;
+        $result = json_decode(shell_exec(str_replace('\\', '', str_replace(PHP_EOL, '', $sh))), true);
+        if(isset($result['richMenuId'])) {
+          return $result['richMenuId'];
+        }
+        else {
+          return $result['message'];
+        }
+      }
+      function getListOfRichmenu($channelAccessToken) {
+        $sh = <<< EOF
+        curl \
+        -H 'Authorization: Bearer $channelAccessToken' \
+        https://api.line.me/v2/bot/richmenu/list;
+      EOF;
+        $result = json_decode(shell_exec(str_replace('\\', '', str_replace(PHP_EOL, '', $sh))), true);
+        return $result;
+      }
+
+
+
+
+
+
+
 
  function replyMsg($arrayHeader,$arrayPostData){
          $strUrl = "https://api.line.me/v2/bot/message/reply";
