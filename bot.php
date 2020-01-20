491e8ea7658e556450043f54a4f2a5c682b1ff95
waenurma <46836897+waenurma@users.noreply.github.com>
2020-01-20 09:03:38 +0700

    button

--- bot.php
+++ bot.php
@@ -26,7 +26,7 @@
 
     //รับข้อความจากผู้ใช้
     $message = $arrayJson['events'][0]['message']['text'];
-    
+    $arrayPostData= array();
  #ตัวอย่าง Message Type "Text"
      if($message == "สวัสดี"){
          $arrayPostData= array();
@@ -187,17 +187,33 @@
         replyMsg($arrayHeader,$arrayPostData);
        
     }
+
+    
     // message ดักจับคำที่แปลกๆ
-    else{
-        $arrayPostData = array();
-        $arrayPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "กรอกข้อมูลไม่ถูกต้อง!!";
-        replyMsg($arrayHeader,$arrayPostData);
+    // else{
+    //     $arrayPostData = array();
+    //     $arrayPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+    //     $arrayPostData['messages'][0]['type'] = "text";
+    //     $arrayPostData['messages'][0]['text'] = "กรอกข้อมูลไม่ถูกต้อง!!";
+    //     replyMsg($arrayHeader,$arrayPostData);
+
+    //  }
+    
+     else if($message == "button"){
+     $actions = array (
+        // general message action
+        New \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("button 1", "text 1"),
+        // URL type action
+        New \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("Google", "http://www.google.com"),
+        // The following two are interactive actions
+        New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("next page", "page=3"),
+        New \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("Previous page", "page=1")
+        );
+        $img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
+        $button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("button text", "description", $img_url, $actions);
+        $outputText = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("Button template builder", $button);
+        $response = $bot->replyMessage($event->getReplyToken(), $outputText);
     }
-
-
-
 
 
 
