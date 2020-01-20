7af7af48b083fed007a0afc22822b20c00eb65cd
waenurma <46836897+waenurma@users.noreply.github.com>
2020-01-20 14:06:18 +0700

    bot

--- bot-flex.php
+++ bot-flex.php
@@ -1,121 +1,158 @@
 <?php
     
-$API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = 'exRwq1i1noogIKE8x9QpmYH8PlQQdSvCjBEeoQfy+sCbKkLHNV3Kol5ZxfuCebtuRyHunNm6/KGAVw+uDgy6GQEAeKsAhLGAIpJCYMLvxVWVX2b4o8DN0z03MVgp1TC2JsjIEQPXRqWxua9JrPIVfwdB04t89/1O/w1cDnyilFU='; 
-$channelSecret = 'aa79f5f6f04e775f836bf54644526aed';
+    $accessToken = "exRwq1i1noogIKE8x9QpmYH8PlQQdSvCjBEeoQfy+sCbKkLHNV3Kol5ZxfuCebtuRyHunNm6/KGAVw+uDgy6GQEAeKsAhLGAIpJCYMLvxVWVX2b4o8DN0z03MVgp1TC2JsjIEQPXRqWxua9JrPIVfwdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+    $content = file_get_contents('php://input');
+    $arrayJson = json_decode($content, true);
+    
+    $arrayHeader = array();
+    $arrayHeader[] = "Content-Type: application/json";
+    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
 
-
-$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
-
-$request = file_get_contents('php://input');   // Get request content
-$request_array = json_decode($request, true);   // Decode JSON to Array
-$jsonFlex = [
-  {
-    "type": "flex",
-    "altText": "Flex Message",
-    "contents": {
-      "type": "bubble",
-      "hero": {
-        "type": "image",
-        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-        "size": "full",
-        "aspectRatio": "20:13",
-        "aspectMode": "cover",
-        "action": {
-          "type": "uri",
-          "label": "Line",
-          "uri": "https://linecorp.com/"
-        }
-      },
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "ผลการเรียน",
-            "size": "xl",
-            "weight": "bold"
-          }
+    $jsonFlex = [
+      "type" => "flex",
+      "altText" => "ผลการเรียน",
+      "contents" => [
+        "type" => "bubble",
+        "direction" => "ltr",
+        "header" => [
+          "type" => "box",
+          "layout" => "vertical",
+          "contents" => [
+            [
+              "type" => "text",
+              "text" => "Purchase",
+              "size" => "lg",
+              "align" => "start",
+              "weight" => "bold",
+              "color" => "#009813"
+            ],
+            [
+              "type" => "text",
+              "text" => "฿ 100.00",
+              "size" => "3xl",
+              "weight" => "bold",
+              "color" => "#000000"
+            ],
+            [
+              "type" => "text",
+              "text" => "Rabbit Line Pay",
+              "size" => "lg",
+              "weight" => "bold",
+              "color" => "#000000"
+            ],
+            [
+              "type" => "text",
+              "text" => "2019.02.14 21:47 (GMT+0700)",
+              "size" => "xs",
+              "color" => "#B2B2B2"
+            ],
+            [
+              "type" => "text",
+              "text" => "Payment complete.",
+              "margin" => "lg",
+              "size" => "lg",
+              "color" => "#000000"
+            ]
+          ]
+        ],
+        "body" => [
+          "type" => "box",
+          "layout" => "vertical",
+          "contents" => [
+            [
+              "type" => "separator",
+              "color" => "#C3C3C3"
+            ],
+            [
+              "type" => "box",
+              "layout" => "baseline",
+              "margin" => "lg",
+              "contents" => [
+                [
+                  "type" => "text",
+                  "text" => "Merchant",
+                  "align" => "start",
+                  "color" => "#C3C3C3"
+                ],
+                [
+                  "type" => "text",
+                  "text" => "BTS 01",
+                  "align" => "end",
+                  "color" => "#000000"
+                ]
+              ]
+            ],
+            [
+              "type" => "box",
+              "layout" => "baseline",
+              "margin" => "lg",
+              "contents" => [
+                [
+                  "type" => "text",
+                  "text" => "New balance",
+                  "color" => "#C3C3C3"
+                ],
+                [
+                  "type" => "text",
+                  "text" => "฿ 45.57",
+                  "align" => "end"
+                ]
+              ]
+            ],
+            [
+              "type" => "separator",
+              "margin" => "lg",
+              "color" => "#C3C3C3"
+            ]
+          ]
+        ],
+        "footer" => [
+          "type" => "box",
+          "layout" => "horizontal",
+          "contents" => [
+            [
+              "type" => "text",
+              "text" => "View Details",
+              "size" => "lg",
+              "align" => "start",
+              "color" => "#0084B6",
+              "action" => [
+                "type" => "uri",
+                "label" => "View Details",
+                "uri" => "https://google.co.th/"
+              ]
+            ]
+          ]
         ]
-      },
-      "footer": {
-        "type": "box",
-        "layout": "vertical",
-        "flex": 0,
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "button",
-            "action": {
-              "type": "uri",
-              "label": "ผลการเรียน 1/63",
-              "uri": "https://linecorp.com"
-            },
-            "height": "sm",
-            "style": "link"
-          },
-          {
-            "type": "button",
-            "action": {
-              "type": "uri",
-              "label": "ผลการเรียน 2/63",
-              "uri": "https://linecorp.com"
-            },
-            "height": "sm",
-            "style": "link"
-          },
-          {
-            "type": "spacer",
-            "size": "xs"
-          }
-        ]
+      ]
+    ];
+  if ( sizeof($request_array['events']) > 0 ) {
+      foreach ($request_array['events'] as $event) {
+          error_log(json_encode($event));
+          $reply_message = '';
+          $reply_token = $event['replyToken'];
+          $data = [
+              'replyToken' => $reply_token,
+              'messages' => [$jsonFlex]
+          ];
+          print_r($data);
+          $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+          $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+          echo "Result: ".$send_result."\r\n";
+          
       }
-    }
   }
-
-
- 
-if ( sizeof($request_array['events']) > 0 ) {
-  foreach ($request_array['events'] as $event) {
-      error_log(json_encode($event));
-      $reply_message = "ผลการเรียน";
-      $reply_token = $event['replyToken'];
-
-
-      $data = [
-          'replyToken' => $reply_token,
-          'messages' => [$jsonFlex]
-      ];
-
-      print_r($data);
-
-      $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-      $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-
-      echo "Result: ".$send_result."\r\n";
-      
+  echo "OK";
+  function send_reply_message($url, $post_header, $post_body)
+  {
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      $result = curl_exec($ch);
+      curl_close($ch);
+      return $result;
   }
-}
-
-echo "OK";
-
-
-
-
-function send_reply_message($url, $post_header, $post_body)
-{
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  $result = curl_exec($ch);
-  curl_close($ch);
-
-  return $result;
-}
-
-?>
+  ?>
