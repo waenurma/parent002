41bfeb62104e67b1dd646819cb7a00ba17e0613b
waenurma <46836897+waenurma@users.noreply.github.com>
2020-01-20 14:31:38 +0700

    บันทึกใหม่

--- bot-flex.php
+++ bot-flex.php
@@ -1,158 +1,48 @@
 <?php
-    
-    $accessToken = "exRwq1i1noogIKE8x9QpmYH8PlQQdSvCjBEeoQfy+sCbKkLHNV3Kol5ZxfuCebtuRyHunNm6/KGAVw+uDgy6GQEAeKsAhLGAIpJCYMLvxVWVX2b4o8DN0z03MVgp1TC2JsjIEQPXRqWxua9JrPIVfwdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
-    $content = file_get_contents('php://input');
-    $arrayJson = json_decode($content, true);
-    
-    $arrayHeader = array();
-    $arrayHeader[] = "Content-Type: application/json";
-    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+ 
+$strAccessToken = "exRwq1i1noogIKE8x9QpmYH8PlQQdSvCjBEeoQfy+sCbKkLHNV3Kol5ZxfuCebtuRyHunNm6/KGAVw+uDgy6GQEAeKsAhLGAIpJCYMLvxVWVX2b4o8DN0z03MVgp1TC2JsjIEQPXRqWxua9JrPIVfwdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่";
+ 
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
+ 
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+ 
+if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
+}else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
+}else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
+}else{
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
+}
+ 
 
-    $jsonFlex = [
-      "type" => "flex",
-      "altText" => "ผลการเรียน",
-      "contents" => [
-        "type" => "bubble",
-        "direction" => "ltr",
-        "header" => [
-          "type" => "box",
-          "layout" => "vertical",
-          "contents" => [
-            [
-              "type" => "text",
-              "text" => "Purchase",
-              "size" => "lg",
-              "align" => "start",
-              "weight" => "bold",
-              "color" => "#009813"
-            ],
-            [
-              "type" => "text",
-              "text" => "฿ 100.00",
-              "size" => "3xl",
-              "weight" => "bold",
-              "color" => "#000000"
-            ],
-            [
-              "type" => "text",
-              "text" => "Rabbit Line Pay",
-              "size" => "lg",
-              "weight" => "bold",
-              "color" => "#000000"
-            ],
-            [
-              "type" => "text",
-              "text" => "2019.02.14 21:47 (GMT+0700)",
-              "size" => "xs",
-              "color" => "#B2B2B2"
-            ],
-            [
-              "type" => "text",
-              "text" => "Payment complete.",
-              "margin" => "lg",
-              "size" => "lg",
-              "color" => "#000000"
-            ]
-          ]
-        ],
-        "body" => [
-          "type" => "box",
-          "layout" => "vertical",
-          "contents" => [
-            [
-              "type" => "separator",
-              "color" => "#C3C3C3"
-            ],
-            [
-              "type" => "box",
-              "layout" => "baseline",
-              "margin" => "lg",
-              "contents" => [
-                [
-                  "type" => "text",
-                  "text" => "Merchant",
-                  "align" => "start",
-                  "color" => "#C3C3C3"
-                ],
-                [
-                  "type" => "text",
-                  "text" => "BTS 01",
-                  "align" => "end",
-                  "color" => "#000000"
-                ]
-              ]
-            ],
-            [
-              "type" => "box",
-              "layout" => "baseline",
-              "margin" => "lg",
-              "contents" => [
-                [
-                  "type" => "text",
-                  "text" => "New balance",
-                  "color" => "#C3C3C3"
-                ],
-                [
-                  "type" => "text",
-                  "text" => "฿ 45.57",
-                  "align" => "end"
-                ]
-              ]
-            ],
-            [
-              "type" => "separator",
-              "margin" => "lg",
-              "color" => "#C3C3C3"
-            ]
-          ]
-        ],
-        "footer" => [
-          "type" => "box",
-          "layout" => "horizontal",
-          "contents" => [
-            [
-              "type" => "text",
-              "text" => "View Details",
-              "size" => "lg",
-              "align" => "start",
-              "color" => "#0084B6",
-              "action" => [
-                "type" => "uri",
-                "label" => "View Details",
-                "uri" => "https://google.co.th/"
-              ]
-            ]
-          ]
-        ]
-      ]
-    ];
-  if ( sizeof($request_array['events']) > 0 ) {
-      foreach ($request_array['events'] as $event) {
-          error_log(json_encode($event));
-          $reply_message = '';
-          $reply_token = $event['replyToken'];
-          $data = [
-              'replyToken' => $reply_token,
-              'messages' => [$jsonFlex]
-          ];
-          print_r($data);
-          $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-          $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-          echo "Result: ".$send_result."\r\n";
-          
-      }
-  }
-  echo "OK";
-  function send_reply_message($url, $post_header, $post_body)
-  {
-      $ch = curl_init($url);
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-      $result = curl_exec($ch);
-      curl_close($ch);
-      return $result;
-  }
-  ?>
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+ 
+?>
