b89833674c3de6fb4e6810c442996af662e90b23
waenurma <46836897+waenurma@users.noreply.github.com>
2020-01-20 11:15:55 +0700

    ลอง03

--- bot-flex.php
+++ bot-flex.php
@@ -1,6 +1,6 @@
 <?php
 
-$API_URL = 'https://api.line.me/v2/bot/message/reply';
+$API_URL = 'https://api.line.me/v2/bot/message';
 $ACCESS_TOKEN = 'exRwq1i1noogIKE8x9QpmYH8PlQQdSvCjBEeoQfy+sCbKkLHNV3Kol5ZxfuCebtuRyHunNm6/KGAVw+uDgy6GQEAeKsAhLGAIpJCYMLvxVWVX2b4o8DN0z03MVgp1TC2JsjIEQPXRqWxua9JrPIVfwdB04t89/1O/w1cDnyilFU='; 
 $channelSecret = 'aa79f5f6f04e775f836bf54644526aed';
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
