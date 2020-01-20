95fba586e0f548334340486923c154a26799465d
waenurma <46836897+waenurma@users.noreply.github.com>
2020-01-20 11:41:35 +0700

    0000

--- bot-flex.php
+++ bot-flex.php
@@ -128,10 +128,10 @@ if ( sizeof($request_array['events']) > ผลการเรียน ) {
         error_log(json_encode($event));
         $reply_message = '';
         $reply_token = $event['replyToken'];
-        $data = [
-            'replyToken' => $reply_token,
-            'messages' => [$jsonFlex]
-        ];
+        // $data = [
+        //     'replyToken' => $reply_token,
+        //     'messages' => [$jsonFlex]
+        // ];
         print_r($data);
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
         $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
