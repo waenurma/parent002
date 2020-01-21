786bbe200668eea4e767a67914ebb55667512fb9
waenurma <46836897+waenurma@users.noreply.github.com>
2020-01-21 09:13:25 +0700

    template

--- bot-flex.php
+++ bot-flex.php
@@ -1,131 +1,35 @@
 <?php
     
     $accessToken = "exRwq1i1noogIKE8x9QpmYH8PlQQdSvCjBEeoQfy+sCbKkLHNV3Kol5ZxfuCebtuRyHunNm6/KGAVw+uDgy6GQEAeKsAhLGAIpJCYMLvxVWVX2b4o8DN0z03MVgp1TC2JsjIEQPXRqWxua9JrPIVfwdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
-    $content = file_get_contents('php://input');
-    $arrayJson = json_decode($content, true);
-    
-    $arrayHeader = array();
-    $arrayHeader[] = "Content-Type: application/json";
-    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+    $API_URL = 'https://api.line.me/v2/bot/message';
+    $channelSecret = 'aa79f5f6f04e775f836bf54644526aed';
+    $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+    $request = file_get_contents('php://input');   // Get request content
+    $request_array = json_decode($request, true);   // Decode JSON to Array
 
     $jsonFlex = [
-      "type" => "flex",
+      "type" => "template",
       "altText" => "ผลการเรียน",
-      "contents" => [
-        "type" => "bubble",
-        "direction" => "ltr",
-        "header" => [
-          "type" => "box",
-          "layout" => "vertical",
-          "contents" => [
-            [
-              "type" => "text",
-              "text" => "Purchase",
-              "size" => "lg",
-              "align" => "start",
-              "weight" => "bold",
-              "color" => "#009813"
-            ],
-            [
-              "type" => "text",
-              "text" => "฿ 100.00",
-              "size" => "3xl",
-              "weight" => "bold",
-              "color" => "#000000"
-            ],
-            [
-              "type" => "text",
-              "text" => "Rabbit Line Pay",
-              "size" => "lg",
-              "weight" => "bold",
-              "color" => "#000000"
-            ],
-            [
-              "type" => "text",
-              "text" => "2019.02.14 21:47 (GMT+0700)",
-              "size" => "xs",
-              "color" => "#B2B2B2"
-            ],
-            [
-              "type" => "text",
-              "text" => "Payment complete.",
-              "margin" => "lg",
-              "size" => "lg",
-              "color" => "#000000"
-            ]
-          ]
+      "template" => [
+        "type"=> "buttons",
+        "actions"=> [
+        [
+            "type" => "massage",
+            "label" => "Action 1",
+            "text" => "ผลการเรียน 1/63"
         ],
-        "body" => [
-          "type" => "box",
-          "layout" => "vertical",
-          "contents" => [
-            [
-              "type" => "separator",
-              "color" => "#C3C3C3"
-            ],
-            [
-              "type" => "box",
-              "layout" => "baseline",
-              "margin" => "lg",
-              "contents" => [
-                [
-                  "type" => "text",
-                  "text" => "Merchant",
-                  "align" => "start",
-                  "color" => "#C3C3C3"
-                ],
-                [
-                  "type" => "text",
-                  "text" => "BTS 01",
-                  "align" => "end",
-                  "color" => "#000000"
-                ]
-              ]
-            ],
-            [
-              "type" => "box",
-              "layout" => "baseline",
-              "margin" => "lg",
-              "contents" => [
-                [
-                  "type" => "text",
-                  "text" => "New balance",
-                  "color" => "#C3C3C3"
-                ],
-                [
-                  "type" => "text",
-                  "text" => "฿ 45.57",
-                  "align" => "end"
-                ]
-              ]
-            ],
-            [
-              "type" => "separator",
-              "margin" => "lg",
-              "color" => "#C3C3C3"
-            ]
+          [
+            "type" => "message",
+            "label" => "Action 2",
+            "text" => "ผลการเรียน 2/63"
+          ],
+        
+        "thumbnailImageUrl" > "SPECIFY_YOUR_IMAGE_URL",
+        "title" => "ผลการเรียน",
+        "text" => "ชื่อ-นามสกุล"
           ]
-        ],
-        "footer" => [
-          "type" => "box",
-          "layout" => "horizontal",
-          "contents" => [
-            [
-              "type" => "text",
-              "text" => "View Details",
-              "size" => "lg",
-              "align" => "start",
-              "color" => "#0084B6",
-              "action" => [
-                "type" => "uri",
-                "label" => "View Details",
-                "uri" => "https://google.co.th/"
-              ]
-            ]
-          ]
-        ]
       ]
-    ];
+        ];
   if ( sizeof($request_array['events']) > 0 ) {
       foreach ($request_array['events'] as $event) {
           error_log(json_encode($event));
