6f44e2cb1f230c602141a35f848474f315b159af
waenurma <46836897+waenurma@users.noreply.github.com>
2020-01-29 14:15:18 +0700

    มั่ว

--- line.php
+++ line.php
@@ -10,7 +10,7 @@
 
 	$messages = [];
 	$messages['replyToken'] = $replyToken;
-	$messages['messages'][0] = getFormatTextMessage("เอ้ย ถามอะไรก็ตอบได้");
+	$messages['messages'][0] = getFormatTextMessage("เอ้ย ถามอะไรก็ตอบได้")||("มั่ว");
 
 	$encodeJson = json_encode($messages);
 
