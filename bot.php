d81988227ba9731b4fa48a1a1d5ddf490abb65ec
waenurma <46836897+waenurma@users.noreply.github.com>
2020-01-29 09:27:21 +0700

    rPostData

--- bot.php
+++ bot.php
@@ -225,20 +225,20 @@ $message = $arrayJson['events'][0]['message']['text'];
    $id = $arrayJson['events'][0]['source']['userId'];
    if($message == "นับ 1-10"){
        for($i=1;$i<=10;$i++){
-          $arrayPostData['to'] = $id;
-          $arrayPostData['messages'][0]['type'] = "text";
-          $arrayPostData['messages'][0]['text'] = $i;
-          pushMsg($arrayHeader,$arrayPostData);
+        $arrPostData['to'] = $id;
+        $arrPostData['messages'][0]['type'] = "text";
+        $arrPostData['messages'][0]['text'] = $i;
+          pushMsg($arrayHeader,$arrPostData);
        }
     }
-   function pushMsg($arrayHeader,$arrayPostData){
+   function pushMsg($arrayHeader,$arrPostData){
       $strUrl = "https://api.line.me/v2/bot/message/push";
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL,$strUrl);
       curl_setopt($ch, CURLOPT_HEADER, false);
       curl_setopt($ch, CURLOPT_POST, true);
       curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
       curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
       $result = curl_exec($ch);
