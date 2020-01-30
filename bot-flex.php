2d9daba92213e79066cb40af5db22dc819c19de8
waenurma <46836897+waenurma@users.noreply.github.com>
2020-01-30 10:41:19 +0700

    3

--- bot-flex.php
+++ bot-flex.php
@@ -147,7 +147,7 @@ if ( sizeof($request_array['events']) > 0 ) {
 
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
-        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+        $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
 
         // echo "Result: ".$send_result."\r\n";
         
