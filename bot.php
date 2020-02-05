954e7fc3c03ffc1db63d06a9ea9c9956a785d195
waenurma <46836897+waenurma@users.noreply.github.com>
2020-02-05 16:09:46 +0700

    ภาษาอังกฤษ

--- bot.php
+++ bot.php
@@ -98,30 +98,33 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี" ||$arrJson['
     $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
 
 
-}else if($arrJson['events'][0]['message']['text'] =="y"){
-    require "imap.php";
+// }else if($arrJson['events'][0]['message']['text'] =="y"){
+//     require "imap.php";
+
+
+
     /////////////////////////////////////////เมนู6เมนู///////////////////////////////////////////////
 
-}else if($arrJson['events'][0]['message']['text'] == "ตารางเรียน"){
+}else if($arrJson['events'][0]['message']['text'] == "ตารางเรียน" || $arrJson['events'][0]['message']['text'] == "timetable"){
     require "butontimetable.php";
     
-}else if($arrJson['events'][0]['message']['text'] == "ผลการเรียน"){
+}else if($arrJson['events'][0]['message']['text'] == "ผลการเรียน" || $arrJson['events'][0]['message']['text'] == "Academic results" || $arrJson['events'][0]['message']['text'] == "grade"){
    require "bot-flex-Gpa.php";
     
     // $replyjson['type'] = 'text'
     // $replyjson['text'] = '1234'
     // json_encode($replyjson)
 
-}else if($arrJson['events'][0]['message']['text'] == "การเรียนการสอน"){
+}else if($arrJson['events'][0]['message']['text'] == "การเรียนการสอน"|| $arrJson['events'][0]['message']['text'] == "learn"){
     require "study.php";
  
-}else if($arrJson['events'][0]['message']['text'] == "การบ้าน"){
+}else if($arrJson['events'][0]['message']['text'] == "การบ้าน"|| $arrJson['events'][0]['message']['text'] == "homework"){
     require "bothomework.php";
     
-}else if($arrJson['events'][0]['message']['text'] == "ค่าใช้จ่าย"){
+}else if($arrJson['events'][0]['message']['text'] == "ค่าใช้จ่าย"|| $arrJson['events'][0]['message']['text'] == "เป๋าตังค์"){
     require "valet.php";
     
-}else if($arrJson['events'][0]['message']['text'] == "กิจกรรม"){
+}else if($arrJson['events'][0]['message']['text'] == "กิจกรรม"|| $arrJson['events'][0]['message']['text'] == "acctivity"){
     require  "buttonActivity.php";
     
 
