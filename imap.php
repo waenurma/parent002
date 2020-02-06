799a56d2d26a5eedad78587a113fefa035f89e54
waenurma <46836897+waenurma@users.noreply.github.com>
2020-02-06 11:28:11 +0700

    ภาพ

--- imap.php
+++ imap.php
@@ -12,7 +12,7 @@ $request_array = json_decode($content , true);   // Decode JSON to Array
 $jsonimagemap = [ 
 
   "type"=> "imagemap",
-  "baseUrl"=> "https://example.com/bot/images/rm001",
+  "baseUrl"=> "https://parent002.herokuapp.com/parent002/i.jpg",
   // "baseUrl"=> "https://charabizasia.files.wordpress.com/2017/07/main-1.jpg?w=1040",
   "altText"=> "This is an imagemap",
   "baseSize"=> [
