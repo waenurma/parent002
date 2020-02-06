be211977ca5af76604f8a2158239d200fea87ebf
waenurma <46836897+waenurma@users.noreply.github.com>
2020-02-06 09:12:50 +0700

    imap

--- imap.php
+++ imap.php
@@ -11,7 +11,7 @@ $request_array = json_decode($content , true);   // Decode JSON to Array
 
 $jsonimagemap = [ 
   "type"=> "imagemap",
-  "baseUrl"=> "PROVIDE_URL_FROM_YOUR_SERVER",
+  "baseUrl"=> "parent002/mission-ico.png",
   "altText"=> "This is an imagemap",
   "baseSize"=> [
     "width"=> 1040,
