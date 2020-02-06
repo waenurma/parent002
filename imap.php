9d946ef63dd587cb93bcd7a489fa1df534270abb
waenurma <46836897+waenurma@users.noreply.github.com>
2020-02-06 09:27:05 +0700

    imap link

--- imap.php
+++ imap.php
@@ -11,7 +11,7 @@ $request_array = json_decode($content , true);   // Decode JSON to Array
 
 $jsonimagemap = [ 
   "type"=> "imagemap",
-  "baseUrl"=> "parent002/mission-ico.png",
+  "baseUrl"=> "https://parent002.herokuapp.com/mission-ico.png",
   "altText"=> "This is an imagemap",
   "baseSize"=> [
     "width"=> 1040,
