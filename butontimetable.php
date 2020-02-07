9ea33ee6bc997ba33f748a0246c4c41773f2493d
waenurma <46836897+waenurma@users.noreply.github.com>
2020-02-07 09:32:56 +0700

    ตารางเรียน

--- butontimetable.php
+++ butontimetable.php
@@ -19,17 +19,17 @@ $jsontemplate = [
             [
               "type"=> "uri",
               "label"=> "ตารางเรียนปัจจุบัน",
-              "uri"=> "http://405965027.student.yru.ac.th/tes5line/homework.php"
+              "uri"=> "http://405965027.student.yru.ac.th/tes5line/studycf.php"
             ],
             [
               "type"=> "uri",
               "label"=> "ตารางเรียนรายวัน",
-              "uri"=> "http://405965027.student.yru.ac.th/tes5line/homework.php"
+              "uri"=> "http://405965027.student.yru.ac.th/tes5line/studycf.php"
             ],
             [
               "type"=> "uri",
               "label"=> "ตารางเรียนร่วม",
-              "uri"=> "http://405965027.student.yru.ac.th/tes5line/homework.php"
+              "uri"=> "http://405965027.student.yru.ac.th/tes5line/studycf.php"
             ]
           ],
           "thumbnailImageUrl"=>  "https://2.bp.blogspot.com/-FwV3GEvNP_0/WiavzB4v2mI/AAAAAAAAARY/lEFa5WR58KcYNucUbwKbUOlctPWsUsroQCLcBGAs/s1600/635817379401360517-School-building-icon.jpg",
