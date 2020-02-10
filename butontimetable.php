d928a41e508cd2e5363a5ab44f8e0507a374169e
waenurma <46836897+waenurma@users.noreply.github.com>
2020-02-10 09:37:46 +0700

    ตารางเรียน

--- butontimetable.php
+++ butontimetable.php
@@ -10,33 +10,37 @@ $content = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($content , true);   // Decode JSON to Array
 
 $jsontemplate = [ 
-    
-        "type"=> "template",
-        "altText"=>  "ตารางเรียน",
-        "template"=>  [
-          "type"=>  "buttons",
-          "actions"=>  [
-            [
-              "type"=> "uri",
-              "label"=> "ตารางเรียนปัจจุบัน",
-              "uri"=> "http://405965027.student.yru.ac.th/tes5line/studycf.php"
-            ],
-            [
-              "type"=> "uri",
-              "label"=> "ตารางเรียนรายวัน",
-              "uri"=> "http://405965027.student.yru.ac.th/tes5line/timetable.php"
-            ],
-            [
-              "type"=> "uri",
-              "label"=> "ตารางเรียนร่วม",
-              "uri"=> "http://405965027.student.yru.ac.th/tes5line/studycf.php"
-            ]
-          ],
-          "thumbnailImageUrl"=>  "https://2.bp.blogspot.com/-FwV3GEvNP_0/WiavzB4v2mI/AAAAAAAAARY/lEFa5WR58KcYNucUbwKbUOlctPWsUsroQCLcBGAs/s1600/635817379401360517-School-building-icon.jpg",
-          "title"=>  "ตารางเรียน",
-          "text"=>  "ชื่อ-สกุล"
+    "type"=> "template",
+    "altText"=> "this is a buttons template",
+    "template"=> [
+      "type"=> "buttons",
+      "actions"=> [
+        [
+          "type"=> "uri",
+          "label"=> "ตารางเรียนถัดไป",
+          "uri"=> "http://405965027.student.yru.ac.th/tes5line/homework.php"
+        ],
+        [
+          "type"=> "uri",
+          "label"=> "ตารางเรียนปัจจุบัน",
+          "uri"=> "http://405965027.student.yru.ac.th/tes5line/homework.php"
+        ],
+        [
+          "type"=> "uri",
+          "label"=> "ตารางเรียนรายวัน",
+          "uri"=> "http://405965027.student.yru.ac.th/tes5line/homework.php"
+        ],
+        [
+          "type"=> "uri",
+          "label"=> "ตารางเรียนร่วม",
+          "uri"=> "http://405965027.student.yru.ac.th/tes5line/homework.php"
         ]
-        ];
+      ],
+      "thumbnailImageUrl"=> "https://2.bp.blogspot.com/-FwV3GEvNP_0/WiavzB4v2mI/AAAAAAAAARY/lEFa5WR58KcYNucUbwKbUOlctPWsUsroQCLcBGAs/s1600/635817379401360517-School-building-icon.jpg",
+      "title"=> "ตารางเรียน",
+      "text"=> "ชื่อ-สกุล"
+    ]
+    ];
 if ( sizeof($request_array['events']) > 0 ) {
     foreach ($request_array['events'] as $event) {
         error_log(json_encode($event));
