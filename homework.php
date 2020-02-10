1d33fa1017b361bda2c7cb2c4e0cc7bb47600fc7
waenurma <46836897+waenurma@users.noreply.github.com>
2020-02-10 15:18:24 +0700

    การบ้าน

---
+++ homework.php
@@ -0,0 +1,69 @@
+<?php
+
+$API_URL = 'https://api.line.me/v2/bot/message';
+$ACCESS_TOKEN = '072ioqcw4uT17+qwjIDmsn4XlTguP6hRKZjWyJf2nu5tFaheu0baLx26OQ3K5II9RyHunNm6/KGAVw+uDgy6GQEAeKsAhLGAIpJCYMLvxVW4aCCAL4XClCPZUtKmZzjBM5mOHHi5w8jFzTfgnDVFc1GUYhWQfeY8sLGRXgo3xvw=';
+$channelSecret = '157d1d03926e37e516f42f5e9a44af73';
+
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+
+$content = file_get_contents('php://input');   // Get request content
+$request_array = json_decode($content , true);   // Decode JSON to Array
+
+$jsontemplate = [
+        "type"=> "template",
+        "altText"=> "การบ้าน",
+        "template"=> [
+          "type"=> "buttons",
+          "actions"=> [
+            [
+              "type"=> "uri",
+              "label"=> "คลิกดูการบ้าน",
+              "uri"=> "http://405965027.student.yru.ac.th/tes5line/homework.php"
+            ]
+          ],
+          "thumbnailImageUrl"=> "https://onlinedegrees.sandiego.edu/wp-content/uploads/2017/12/education_inequity_and_homework.jpg",
+          "title"=> "การบ้าน",
+          "text"=> "ดูรายละเอียดเพิ่ม"
+        ]
+        ];
+if ( sizeof($request_array['events']) > 0 ) {
+    foreach ($request_array['events'] as $event) {
+        error_log(json_encode($event));
+        $reply_message = '';
+        $reply_token = $event['replyToken'];
+
+        $data = [
+            'replyToken' => $reply_token,
+            'messages' => [$jsontemplate ]
+        ];
+
+        print_r($data);
+
+        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+        
+        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+
+        echo "Result: ".$send_result."\r\n";
+        
+    }
+}
+
+echo "OK";
+
+
+function send_reply_message($url, $post_header, $post_body)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
+
+
+?>
