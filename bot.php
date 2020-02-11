c86dbfb2db4bdf78c142a4b16f3c4f8dff03a81d
waenurma <46836897+waenurma@users.noreply.github.com>
2020-02-11 11:39:07 +0700

    ชื่อ

--- bot.php
+++ bot.php
@@ -15,7 +15,7 @@ if($arrJson['events'][0]['message']['text'] == "ID"){
     $arrPostData = array();
     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
     $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$userData['displayName'];  
+    $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId']['username'];
    
 }else if($arrJson['events'][0]['message']['text'] == "สวัสดี" ||$arrJson['events'][0]['message']['text'] == "สวัสดีค่ะ" || $arrJson['events'][0]['message']['text'] == "สวัสดีคะ"){
     $arrPostData = array();
