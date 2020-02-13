bbb57646794729e61d5f9e5b6c5ad217eff680c0
waenurma <46836897+waenurma@users.noreply.github.com>
2020-02-13 11:11:34 +0700

    เกรด

--- bot.php
+++ bot.php
@@ -91,6 +91,8 @@ if($arrJson['events'][0]['message']['text'] == "ID"||$arrJson['events'][0]['mess
 }else if($arrJson['events'][0]['message']['text'] == "เป๋าตังค์" ||$arrJson['events'][0]['message']['text'] == "ค่าใช้จ่าย" ){
     require "vollet.php";
    
+}else if($arrJson['events'][0]['message']['text'] == "เกรด" ){
+    require "flex.php";
 
 
 
