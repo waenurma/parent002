2bcaee0873145e899daf500d018512e4a1d94ffa
waenurma <46836897+waenurma@users.noreply.github.com>
2020-02-17 12:10:55 +0700

    17/02/63

--- bot.php
+++ bot.php
@@ -91,7 +91,7 @@ if($arrJson['events'][0]['message']['text'] == "ID"||$arrJson['events'][0]['mess
 }else if($arrJson['events'][0]['message']['text'] == "เป๋าตังค์" ||$arrJson['events'][0]['message']['text'] == "ค่าใช้จ่าย" ){
     require "vollet.php";
    ////ผลเรียนแบบflexตัวใหญ่
-}else if($arrJson['events'][0]['message']['text'] == "เกรด" ){
+}else if($arrJson['events'][0]['message']['text'] == "เกรด"||$arrJson['events'][0]['message']['text'] == "ผลการเรียน" ){
     require "flex.php";
 }else if($arrJson['events'][0]['message']['text'] == "a" ){
     require "test.php";
