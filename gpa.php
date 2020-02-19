67ec85e6d7175cb00114abf3b0e13c85df88b1b7
waenurma <46836897+waenurma@users.noreply.github.com>
2020-02-19 11:11:06 +0700

    เกรด

--- gpa.php
+++ gpa.php
@@ -1,8 +1,43 @@
-<ion-header>
-    <ion-toolbar color="secondary">
-        <ion-buttons slot="start">
-            <ion-back-button></ion-back-button>
-        </ion-buttons>
-        <ion-title class="menu-title">เข้าสู่ระบบ</ion-title>
-    </ion-toolbar>
-</ion-header>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+.navbar {
+  overflow: hidden;
+  background-color: #333; 
+}
+
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+
+.navbar a:hover, .subnav:hover .subnavbtn {
+  background-color: red;
+}
+
+
+</style>
+</head>
+<body>
+
+<div class="navbar">
+  <a href="#home">ผลการเรียน</a>
+  <div class="subnav">
+   
+
+
+</body>
+</html>
