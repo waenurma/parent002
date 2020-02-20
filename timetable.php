774d0d79b403efd4ebb15b5f1258a684e396b196
waenurma <46836897+waenurma@users.noreply.github.com>
2020-02-20 13:44:05 +0700

    ตารางเรียนร่วม

--- timetable.php
+++ timetable.php
@@ -33,7 +33,7 @@ $jsontemplate = [
         [
           "type"=> "uri",
           "label"=> "ตารางเรียนร่วม",
-          "uri"=> "http://405965027.student.yru.ac.th/tes5line/homework.php"//เปลี่ยนลิงค์
+          "uri"=> "http://405965027.student.yru.ac.th/tes5line/study_joi.php"//เปลี่ยนลิงค์
         ]
       ],
       "thumbnailImageUrl"=> "https://2.bp.blogspot.com/-FwV3GEvNP_0/WiavzB4v2mI/AAAAAAAAARY/lEFa5WR58KcYNucUbwKbUOlctPWsUsroQCLcBGAs/s1600/635817379401360517-School-building-icon.jpg",
