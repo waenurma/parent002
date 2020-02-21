23edc82b9cccf8597951aba3a5291002e6a8b28b
waenurma <46836897+waenurma@users.noreply.github.com>
2020-02-21 10:11:51 +0700

    เกรด

--- bot.php
+++ bot.php
@@ -101,8 +101,8 @@ if($arrJson['events'][0]['message']['text'] == "ID"||$arrJson['events'][0]['mess
 }else if($arrJson['events'][0]['message']['text'] == "เป๋าตังค์" ||$arrJson['events'][0]['message']['text'] == "ค่าใช้จ่าย" ){
     require "vollet.php";
    ////ผลเรียนแบบflexตัวใหญ่
-}else if($arrJson['events'][0]['message']['text'] == "เกรด"||$arrJson['events'][0]['message']['text'] == "ผลการเรียน" ){
-    // require "gpa.php";
+// }else if($arrJson['events'][0]['message']['text'] == "เกรด"||$arrJson['events'][0]['message']['text'] == "ผลการเรียน" ){
+//     require "gpa.php";
     
 // }else if($arrJson['events'][0]['message']['text'] == "a" ){
 //     require "test.php";
