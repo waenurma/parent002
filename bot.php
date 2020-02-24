1e9e5b2f181cc71ea7379dd5104f9528fc7e5283
waenurma <46836897+waenurma@users.noreply.github.com>
2020-02-24 16:11:40 +0700

    poatang

--- bot.php
+++ bot.php
@@ -99,7 +99,7 @@ if($arrJson['events'][0]['message']['text'] == "ID"||$arrJson['events'][0]['mess
     require "homework.php";
 
 }else if($arrJson['events'][0]['message']['text'] == "เป๋าตังค์" ||$arrJson['events'][0]['message']['text'] == "ค่าใช้จ่าย" ){
-    require "gpa.php";
+    require "poatang.php";
    ////ผลเรียนแบบflexตัวใหญ่
 // }else if($arrJson['events'][0]['message']['text'] == "เกรด"||$arrJson['events'][0]['message']['text'] == "ผลการเรียน" ){
 //     require "gpa.php";
