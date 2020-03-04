4410dfd75b71fa48fb53766b51aba7489ba48138
waenurma <46836897+waenurma@users.noreply.github.com>
2020-03-04 16:25:53 +0700

    04/03/63-1

--- flaxmessage copy.php
+++ flaxmessage copy.php
@@ -116,9 +116,7 @@ $jsonFlex = [
       ]
     ]
   ];
-
-
-
+  
 if ( sizeof($request_array['events']) > 0 ) {
     foreach ($request_array['events'] as $event) {
         error_log(json_encode($event));
