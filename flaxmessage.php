9efde0db6698fb25977f65e350d9d888ae461268
waenurma <46836897+waenurma@users.noreply.github.com>
2020-03-05 11:06:57 +0700

    6

--- flaxmessage.php
+++ flaxmessage.php
@@ -1,21 +1,16 @@
 <?php
 
 include('config.php');
-/*
+
 $API_URL = 'https://api.line.me/v2/bot/message';
 $channelSecret = '157d1d03926e37e516f42f5e9a44af73';
 $ACCESS_TOKEN = '072ioqcw4uT17+qwjIDmsn4XlTguP6hRKZjWyJf2nu5tFaheu0baLx26OQ3K5II9RyHunNm6/KGAVw+uDgy6GQEAeKsAhLGAIpJCYMLvxVW4aCCAL4XClCPZUtKmZzjBM5mOHHi5w8jFzTfgnDVFc1GUYhWQfeY8sLGRXgo3xvw=';
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
-*/
+
 $link = URL .'api/apiparent/show_subjgrade.php?system=school&id=0001&student=01658&card=1959900766962&action=subjgrade';
 
 $result1 = file_get_contents($link);
 $request_array1  = (json_decode($result1, true));
-//var_dump($request_array1);
-echo "1".$request_array1[0]['data'][0]['GPA_ALL'];
-echo "2".$request_array1['data'][0]['GPA_ALL'];
-/*
-
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 
@@ -39,7 +34,7 @@ $jsonFlex = [
         ],
         [
           "type" => "text",
-          "text" => $request_array1['data'][0]['GPA_ALL'],
+          "text" => $request_array1[0]['data'][0]['GPA_ALL'],
           "flex"=> 0,
           "margin"=> "lg",
           "size"=> "md",
@@ -175,5 +170,5 @@ function send_reply_message($url, $post_header, $post_body)
 
     return $result;
 }
-*/
+
 ?>
