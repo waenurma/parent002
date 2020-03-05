877c813870743fa0bc85ddb7758b942a876aa057
waenurma <46836897+waenurma@users.noreply.github.com>
2020-03-05 14:06:58 +0700

    2

--- flaxmessage.php
+++ flaxmessage.php
@@ -145,19 +145,20 @@ if ( sizeof($request_array ['events']) > 0 ) {
             'replyToken' => $reply_token,
             'messages' => [$jsonFlex]
         ];
-var_dump($data);
-        print_r($data);
+
 
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
      //   $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-echo "<pre>",print_r($post_body,true),"</pre>";
+
         echo "Result: ".$send_result."\r\n";
         
     }
 }
-
-echo "OK";
+echo "<pre>",print_r($post_body,true),"</pre>";
+echo var_dump($data);
+        print_r($data);
+//echo "OK";
 /*
 function send_reply_message($url, $post_header, $post_body)
 {
