1ff0167e9efc552b0bbf70403d24f3ac58bca5fc
waenurma <46836897+waenurma@users.noreply.github.com>
2020-03-11 09:11:50 +0700

    เช็ค

--- bot.php
+++ bot.php
@@ -95,7 +95,7 @@ if($arrJson['events'][0]['message']['text'] == "ID"||$arrJson['events'][0]['mess
 }else if($arrJson['events'][0]['message']['text'] == "เกรด"||$arrJson['events'][0]['message']['text'] == "ผลการเรียน" ){
     require "gpa2.php";
 
-}else if($arrJson['events'][0]['message']['text'] == "การมาเรียน" ){
+}else if($arrJson['events'][0]['message']['text'] == "การมาเรียน"||$arrJson['events'][0]['message']['text'] == "เช็ค" ){
     require "Check.php";
 
 
