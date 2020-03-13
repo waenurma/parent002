04a431f5aefc5470d3e391811951f61bbf7f4775
waenurma <46836897+waenurma@users.noreply.github.com>
2020-03-13 14:35:44 +0700

    ttfg

--- linebot/connect.php
+++ linebot/connect.php
@@ -12,7 +12,7 @@ include 'pgconfiq.php';
    
     // $id_card = $_REQUEST["pass"];
     $user_id = $_REQUEST["user_id"];
-         $sql1="INSERT INTO linebot(user_id,id_card)values('$user_id','$data->id_card')";
+         $sql1="INSERT INTO public.linebot(user_id,id_card)values('$user_id','$data->id_card')";
          $stmt = $dbConnection->prepare($sql1);
          // $stmt->bindValue($data->id_card,PDO::PARAM_STR);
          $stmt->execute();
