4bda310af003c402d0c0888ce69f08f2bec97fed
waenurma <46836897+waenurma@users.noreply.github.com>
2020-03-18 15:14:46 +0700

    commit3

--- linebot/connect.php
+++ linebot/connect.php
@@ -1,14 +1,17 @@
 <?php 
 include 'pgconfiq.php';
 
+
  $strSQL=('SELECT * FROM public.login WHERE id_card=:id_card' );
  $stmt = $dbConnection->prepare($strSQL);
  $stmt->bindValue(":id_card", $data->id_card,PDO::PARAM_STR);
  $stmt->execute();
  $result = $stmt -> fetch();
 
+
  if($data->id_card == $result['id_card']){
  
+
     // $id_card = $_REQUEST["pass"];
     $user_id = $_REQUEST["user_id"];
          $sql1="INSERT INTO public.linebot(user_id,id_card)values('$user_id','$data->id_card')";
@@ -16,4 +19,6 @@ include 'pgconfiq.php';
          // $stmt->bindValue($data->id_card,PDO::PARAM_STR);
          $stmt->execute();
          $result = $stmt -> fetch();}
+
+         
 ?>
