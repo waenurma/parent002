46dce3d16447922e513c42616ae7d601b84c26b4
waenurma <46836897+waenurma@users.noreply.github.com>
2020-03-30 14:49:10 +0700

    commit;

--- linebot/cont.php
+++ linebot/cont.php
@@ -1,11 +1,11 @@
 <?php 
 include 'pgconfiq.php';
-
- $strSQL=('SELECT * FROM public.linebot where user_id = "'.$_REQUEST["user_id"].'" ' );
+echo 'SELECT * FROM public.linebot where user_id = "'.$_REQUEST["user_id"].'" ';
+ /*$strSQL=('SELECT * FROM public.linebot where user_id = "'.$_REQUEST["user_id"].'" ' );
  $stmt = $dbConnection->prepare($strSQL);
  $stmt->execute();
  $result = $stmt -> fetch();
  
  print_r($stmt);
-         
+ */        
 ?>
