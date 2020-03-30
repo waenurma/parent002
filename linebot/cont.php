f3f8d91245c64d5e5596e53542c6c3b1bc4110d7
waenurma <46836897+waenurma@users.noreply.github.com>
2020-03-30 14:42:48 +0700

    commit

--- linebot/cont.php
+++ linebot/cont.php
@@ -3,7 +3,6 @@ include 'pgconfiq.php';
 
  $strSQL=('SELECT * FROM public.login ' );
  $stmt = $dbConnection->prepare($strSQL);
- $stmt->bindValue(":id_card", $data->id_card,PDO::PARAM_STR);
  $stmt->execute();
  $result = $stmt -> fetch();
  
